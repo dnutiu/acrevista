621b62bcc881c128ccecfe0fbce5dd267358d72c
Denis Nuțiu <denis.nutiu@gmail.com>
2017-06-24 21:08:08 +0300

    Tidying up

--- account/views.py
+++ account/views.py
@@ -16,7 +16,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return HttpResponse('Authenticated successfully') # TODO: Redirect via render()
                 else:
                     return HttpResponse('Disabled account')
             else:
