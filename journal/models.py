10adf26df287bb20b7d507979952adbf4f85fa29
Denis Nuțiu <denis.nutiu@gmail.com>
2017-07-04 16:32:36 +0300

    Fixing small pep8 warnings and adding comments

--- journal/models.py
+++ journal/models.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 from .validators import FileValidator
 
 
+# This function is used by the Paper model class.
 def user_id_path(instance, filename):
+    """
+    Constructs a path of the type /papers/<user_id>_<username>/<paper title>/<filename>
+
+    instance: The instance provide dy django
+    filename: The filename provided by django
+
+    returns: A string containing the path of the uploaded paper.
+    """
     return "papers/{0}_{3}/{1}/{2}".format(instance.user.id, instance.title, filename, instance.user.username)
 
 
@@ -14,9 +23,11 @@ class Paper(models.Model):
     # File validator.
     validate_file = FileValidator(max_size=1024 * 1024 * 50,  # Max size is 50Mb
                                   content_types=('application/pdf', 'text/html', 'application/msword',
-                                                 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                                                 'application/vnd.openxmlformats-officedocument.wordprocessingml.'
+                                                 'document',
                                                  'application/vnd.ms-powerpoint',
-                                                 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                                                 'application/vnd.openxmlformats-officedocument.presentationml.'
+                                                 'presentation',
                                                  'text/plain',))
     manuscript = models.FileField(upload_to=user_id_path, blank=False, validators=[validate_file])
     cover_letter = models.FileField(upload_to=user_id_path, blank=False, validators=[validate_file])
