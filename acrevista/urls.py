63e6d0f748aadeffc120e27ba9641c7b9fca2a8b
Denis Nuțiu <denis.nutiu@gmail.com>
2017-07-06 14:24:03 +0300

    Fixing some urls and changing theme

--- acrevista/urls.py
+++ acrevista/urls.py
@@ -16,16 +16,17 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.static import serve
+from journal import views as journal_views
 from . import settings
 
 handler404 = 'account.views.custom_404'
 urlpatterns = [
+    url(r'^$', journal_views.homepage, name="home"),  # Journal Homepage :)
     url(r'^admin/', admin.site.urls),
     # Fixme: We can remove the bellow line when we fix namespace error for Django's password reset and password reset done view.
     url(r'^account/', include('account.urls')),
     url(r'^account/', include('account.urls', namespace='account', app_name='account')),
     url(r'^journal/', include('journal.urls', namespace='journal', app_name='journal')),
-    url(r'^', include('journal.urls')),  # Journal Homepage :)
     # Not suitable for production. https://docs.djangoproject.com/en/dev/howto/static-files/deployment/
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 ]
