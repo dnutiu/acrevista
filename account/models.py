e0d2bc044987a7bc0b562e4f1ef66608dac74165
Denis Nuțiu <denis.nutiu@gmail.com>
2017-10-05 13:09:14 +0300

    Implementing LoginToken model

--- account/models.py
+++ account/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.conf import settings
+from . import utilities
+
+# The LoginToken class provides password less login.
+class LoginToken(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    token = models.CharField(max_length=64, default=utilities.generate_security_token)
+    expiry_date = models.DateTimeField(default=utilities.days_from_current_time)
 
 
 # The Profile class extends Django's default user model.
---
+++ account/utilities.py
@@ -0,0 +1,27 @@
+"""
+This file will provide various utility functions.
+"""
+import base64
+import os
+import datetime
+
+
+def generate_security_token(size=32):
+    """
+    This function will generate a random token that is url safe.
+    :param size: The size of the token. Max size is 64 and default size is 32.
+    :return: The generated token.
+    """
+    if size > 64:
+        raise ValueError("The maximum size for generate_security_token is 32 bytes!")
+
+    return base64.urlsafe_b64encode(os.urandom(size)).rstrip(b'=').decode('ascii')
+
+
+def days_from_current_time(days=10):
+    """
+    Computes a date containing n days from the current time.
+    :param days: The number of days.
+    :return: The computed date.
+    """
+    return datetime.datetime.now() + datetime.timedelta(days=10)
