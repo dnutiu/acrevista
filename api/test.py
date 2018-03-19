63dd7afb6c3fb612fc50fde63f16164f6a7fc7bb
Denis Nutiu <denis.nutiu@gmail.com>
2018-03-19 09:25:23 +0200

    Uncomment token code validation it tests

--- api/test.py
+++ api/test.py
@@ -27,11 +27,11 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         user = User.objects.latest('id')
-        # token = Token.objects.get(user=user)
+        token = Token.objects.get(user=user)
         self.assertEqual(User.objects.count(), 2)
 
         self.assertEqual(response.data['email'], data['email'])
-        # self.assertEqual(response.data['token'], token.key)
+        self.assertEqual(response.data['token'], token.key)
         self.assertFalse('password' in response.data)
 
     def test_create_user_with_short_password(self):
