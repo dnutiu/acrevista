9bb091b4ca20d85747fd1ff73cd411d69c7a73e1
Denis Nuțiu <denis.nutiu@gmail.com>
2018-06-07 16:01:50 +0300

    Implementing automatic status update

--- journal/models.py
+++ journal/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 from django.urls import reverse
 from .validators import FileValidator
 from .mail import send_mail_paper_status_update
+from django.db.models.signals import post_save, pre_save
 
 
 # This function is used by the Paper model class.
@@ -33,10 +35,10 @@ def review_user_id_path(instance, filename):
 
 # File validator.
 JOURNAL_PAPER_FILE_VALIDATOR = FileValidator(max_size=1024 * 1024 * 50,  # Max size is 50Mb
-                              content_types=('application/pdf', 'text/html', 'application/msword',
-                                             'application/vnd.openxmlformats-officedocument.wordprocessingml.'
-                                             'document',
-                                             'text/plain',))
+                                             content_types=('application/pdf', 'text/html', 'application/msword',
+                                                            'application/vnd.openxmlformats-officedocument.wordprocessingml.'
+                                                            'document',
+                                                            'text/plain',))
 
 
 class Paper(models.Model):
@@ -69,14 +71,6 @@ class Paper(models.Model):
     def get_absolute_url(self):
         return reverse('journal:paper_detail', args=[str(self.id)])
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        """Override of the save method in order to send email notifications when the status of the paper chances."""
-        if self.pk is not None:
-            original = Paper.objects.get(pk=self.pk)
-            if original.status != self.status:
-                send_mail_paper_status_update(self.title, original.status, self.status)
-        super(Paper, self).save()
-
     def __str__(self):
         return self.title
 
@@ -107,10 +101,38 @@ class Review(models.Model):
     confidential_comment = models.TextField(max_length=32768,
                                             help_text="This comment will not be shown to the author.")
 
-    additional_file = models.FileField(upload_to=review_user_id_path, blank=True, validators=[JOURNAL_PAPER_FILE_VALIDATOR])
+    additional_file = models.FileField(upload_to=review_user_id_path, blank=True,
+                                       validators=[JOURNAL_PAPER_FILE_VALIDATOR])
 
     class Meta:
         ordering = ('created',)
 
     def __str__(self):
         return "{}'s review of {}".format(self.user.username, self.paper.title)
+
+
+@receiver(pre_save, sender=Paper)
+def editor_field_changed(sender, instance, **kwargs):
+    """
+        If there's an editor and paper status is processing. Change the status to under_review.
+        If there's no editor and the paper status is under_review, change the status to processing.
+    """
+    if instance.editor and instance.status == Paper.STATUS_CHOICES[0][0]:
+        instance.status = Paper.STATUS_CHOICES[1][0]  # under_review
+    elif instance.editor == None and instance.status == Paper.STATUS_CHOICES[1][0]:
+        instance.status = Paper.STATUS_CHOICES[0][0]  # processing
+
+
+@receiver(post_save, sender=Review)
+def reviews_changed(sender, instance, **kwargs):
+    """
+        If there's an editor review, set the paper status according to the result of the editor review.
+    """
+    if instance.editor_review:
+        # If the editor review is positive, change the paper status to published.
+        if instance.appropriate == Review.APPROPRIATE_CHOICES[0][0] \
+                and instance.recommendation == Review.RECOMMENDATION_CHOICES[1][0]:
+            instance.paper.status = Paper.STATUS_CHOICES[3][0]  # accepted
+        else:
+            instance.paper.status = Paper.STATUS_CHOICES[2][0]  # preliminary_reject
+        instance.paper.save()
